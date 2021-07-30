0b44c724f9672bda4981814068f8a4f7f804b406
Sean Lee <sean@Seans-Air.lan>
2021-07-30 19:32:44 +1000

    Add Kubernetes CronJob for crawler

--- bin/delete-docker-container.sh
+++ bin/delete-docker-container.sh
@@ -6,3 +6,4 @@ docker rm -f myblog-search-nginx
 docker rm -f myblog-search-vespa
 docker volume rm myblog-search_vespa-logs
 docker volume rm myblog-search_vespa-var
+docker volume rm myblog-search_crawler-backup
--- bin/deploy-kubernetes.sh
+++ bin/deploy-kubernetes.sh
@@ -2,6 +2,10 @@
 
 kubectl create -f kubernetes/myblog-search-env.yaml
 
+PROVISIONER=`kubectl get sc | grep '\(default\)' | awk '{print $3}' | cut -d/ -f2`
+
+kubectl create -f kubernetes/myblog-search-sc-$PROVISIONER.yaml
+
 kubectl create -f vespa/vespa.yaml
 
 while [[ $(kubectl get pods -l app=myblog-search -l name=vespa -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}' | sort -u) != "True" ]]; do echo "waiting for vespa configserver" && sleep 10; done
@@ -18,10 +22,16 @@ kubectl exec vespa-0 -- bash -c '/opt/vespa/bin/vespa-deploy prepare /applicatio
 
 kubectl exec vespa-0 -- bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8080/ApplicationStatus)" != "200" ]]; do echo "waiting for vespa container" && sleep 10; done'
 
-kubectl create -f crawler/crawler.yaml
+kubectl create -f crawler/crawler-pvc.yaml
+
+kubectl create -f crawler/crawler-batch.yaml
 
 echo "sending a test query to vespa server"
 kubectl exec vespa-0 -- bash -c 'for i in {1..10}; do sleep 10 && curl -s "http://localhost:8080/search/?query=fish" | grep -o "\"totalCount\":[1-9]"; if [ $? -eq 0 ]; then echo "successful" && break; fi; echo "retry..."; done'
+
+kubectl delete -f crawler/crawler-batch.yaml
+
+kubectl create -f crawler/crawler-cronjob.yaml
 
 kubectl create -f middleware/middleware.yaml
 
